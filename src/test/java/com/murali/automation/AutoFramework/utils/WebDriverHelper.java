9bcb3c762f481b373a31362e5711956517ac2525
Deepthi <tech.naveenks@gmail.com>
2022-11-25 08:42:39 +0530

    updated WebDriverHelper

--- src/test/java/com/murali/automation/AutoFramework/utils/WebDriverHelper.java
+++ src/test/java/com/murali/automation/AutoFramework/utils/WebDriverHelper.java
@@ -1,5 +1,23 @@
 package com.murali.automation.AutoFramework.utils;
 
-public class WebDriverHelper {
+import java.io.File;
+import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriverException;
+
+public class WebDriverHelper extends BaseTest{
+	public static void getScreenShot(String value) throws IOException, WebDriverException {
+		String path = System.getProperty("user.dir");
+		File scrFile = null;
+		try {
+			scrFile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
+		} catch (WebDriverException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		FileUtils.copyFile(scrFile, new File("./screenshots/" + value + ".png"));
+	}
 }
